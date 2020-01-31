bf25a85569d39e93d2d619b0198482771e6fdf90
danikhani <30578398+danikhani@users.noreply.github.com>
2020-01-31 21:04:58 +0100

    One page programm works fine. #TODO: In order to have two page programm in a structred way. I will use the object oriented way to restructure the whole programm

--- Main.py
+++ Main.py
@@ -6,29 +6,48 @@ import Importer as importer
 HEIGHT = 700
 WIDTH = 500
 
-root = tk.Tk()
 
+def button_clicked():
+    print("you clicked")
+
+
+# Beginning of the firstpage
+
+root = tk.Tk()
+root.title('Korean Recipe')
+
+# A canvas for a default size while starting
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
+# In this frame the other objects will be added
 frame = tk.Frame(root, bg='white')
 frame.place(  relwidth=1, relheight=1)
 
+# the image of the foods will be called from the get_food_image function of repoter module
 food_image = tk.PhotoImage(file=importer.get_food_image(2))
 food_label = tk.Label(frame, image = food_image)
-food_label.place(relx=0.1, rely=0.3, relwidth=0.8, relheight=0.5)
+food_label.place(relx=0.5, rely=0.3, width=400, height=400, anchor = 'n')
 
-button2 = tk.Button(frame, text = 'Show the recipe')
-button2.place(relx=0.1, rely=0.9, relwidth=0.8, relheight=0.05)
+# Button to close this and go to the next page
+button2 = tk.Button(frame, text='Show the recipe', command=button_clicked)
+button2.place(relx=0.5, rely=0.9, relwidth=0.8, relheight=0.05, anchor = 'n')
+
+# Its the code for dropdown menu
+dropdown_selected = tk.StringVar()
+dropdown_selected.set("Select the desired recipe")  # Default value
+food_list = ("yoyoyo",2,3)
 
 
-list_of_food = ttk.Combobox(frame, state='readonly')
-list_of_food['values'] = ("Plesae select a food",1,2,3)
-list_of_food.current(0)
-list_of_food.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.05)
+def option_changed(*args):
+    print("the user chose the value {}".format(dropdown_selected.get()))
 
-#button = tk.Button(frame, text = 'test')
-#button.pack(side='bottom', fill='x')
+list_of_food = tk.OptionMenu(root, dropdown_selected, *food_list)
+# StringVar().trace() : https://kite.com/python/docs/Tkinter.StringVar.trace
+dropdown_selected.trace("w", option_changed)
+list_of_food.place(relx=0.5, rely=0.1, relwidth=0.8, relheight=0.05, anchor = 'n')
+
+
 
 
 root.mainloop()
