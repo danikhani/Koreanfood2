9f109fa9742af513514c6133f3287bbfd73f067c
danikhani <30578398+danikhani@users.noreply.github.com>
2020-02-01 10:21:36 +0100

    Made a object oriented structure from the functional.

--- Main.py
+++ Main.py
@@ -3,51 +3,53 @@ import tkinter.ttk as ttk
 from pathlib import Path
 import Importer as importer
 
-HEIGHT = 700
-WIDTH = 500
 
+class Mainpage:
+    def __init__(self, master):
+        self.HEIGHT = 700
+        self.WIDTH = 500
+        self.master = master
 
-def button_clicked():
-    print("you clicked")
+        # A canvas for a default size while starting
+        self.canvas = tk.Canvas(self.master, height=self.HEIGHT, width=self.WIDTH)
+        self.canvas.pack()
 
+        # In this frame the other objects will be added
+        self.frame = tk.Frame(self.master, bg='white')
+        self.frame.place(relwidth=1, relheight=1)
 
-# Beginning of the firstpage
+        # the image of the foods will be called from the get_food_image function of importer module
+        self.food_image = tk.PhotoImage(file=importer.get_food_image(2))
+        self.food_label = tk.Label(self.frame, image=self.food_image)
+        self.food_label.place(relx=0.5, rely=0.3, width=400, height=400, anchor='n')
 
-root = tk.Tk()
-root.title('Korean Recipe')
+        # Button to close this and go to the next page
+        self.button2 = tk.Button(self.frame, text='Show the recipe', command=self.button_clicked)
+        self.button2.place(relx=0.5, rely=0.9, relwidth=0.8, relheight=0.05, anchor='n')
 
-# A canvas for a default size while starting
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-canvas.pack()
+        # Its the code for dropdown menu
+        self.dropdown_selected = tk.StringVar()
+        self.dropdown_selected.set("Select the desired recipe")  # Default value
+        self.food_list = ("yoyoyo", 2, 3)
 
-# In this frame the other objects will be added
-frame = tk.Frame(root, bg='white')
-frame.place(  relwidth=1, relheight=1)
+        self.list_of_food = tk.OptionMenu(self.frame, self.dropdown_selected, *self.food_list)
+        # StringVar().trace() : https://kite.com/python/docs/Tkinter.StringVar.trace
+        self.dropdown_selected.trace("w", self.option_changed)
+        self.list_of_food.place(relx=0.5, rely=0.1, relwidth=0.8, relheight=0.05, anchor='n')
 
-# the image of the foods will be called from the get_food_image function of repoter module
-food_image = tk.PhotoImage(file=importer.get_food_image(2))
-food_label = tk.Label(frame, image = food_image)
-food_label.place(relx=0.5, rely=0.3, width=400, height=400, anchor = 'n')
+    def button_clicked(self):
+        print("you clicked")
 
-# Button to close this and go to the next page
-button2 = tk.Button(frame, text='Show the recipe', command=button_clicked)
-button2.place(relx=0.5, rely=0.9, relwidth=0.8, relheight=0.05, anchor = 'n')
-
-# Its the code for dropdown menu
-dropdown_selected = tk.StringVar()
-dropdown_selected.set("Select the desired recipe")  # Default value
-food_list = ("yoyoyo",2,3)
-
-
-def option_changed(*args):
-    print("the user chose the value {}".format(dropdown_selected.get()))
-
-list_of_food = tk.OptionMenu(root, dropdown_selected, *food_list)
-# StringVar().trace() : https://kite.com/python/docs/Tkinter.StringVar.trace
-dropdown_selected.trace("w", option_changed)
-list_of_food.place(relx=0.5, rely=0.1, relwidth=0.8, relheight=0.05, anchor = 'n')
+    def option_changed(self, *args):
+        print("the user chose the value {}".format(self.dropdown_selected.get()))
 
 
 
+def main():
+    root = tk.Tk()
+    root.title('Korean Recipe')
+    app = Mainpage(root)
+    root.mainloop()
 
-root.mainloop()
+if __name__ == '__main__':
+    main()
