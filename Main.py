4691e351d71dd1850790cf8d71cbe832af7aa46e
danikhani <30578398+danikhani@users.noreply.github.com>
2020-02-02 14:17:36 +0100

    changed the whole structure of the programm. Its now using 'bringing frames up' method to show the new pages: https://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import font as tkfont
 import gui as gui
 import Importer as importer
 from Timer.Timer_gui import View
@@ -6,15 +7,44 @@ import tkinter.ttk as ttk
 from pathlib import Path
 
 
-def main():
-    root = tk.Tk()
-    root.title('Korean Recipe')
-    app = gui.Mainpage(root)
-    root.mainloop()
+class Main(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        self.geometry("500x700")  # You want the size of the app to be 500x500
+        self.resizable(0, 0)  # Don't allow resizing in the x or y direction
+        self.title('Korean Food')
+
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (gui.Mainpage, gui.Secondpage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("Mainpage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
 
 
-    #view = View()
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    app = Main()
+    app.mainloop()
--- gui.py
+++ gui.py
@@ -6,27 +6,22 @@ import gui_utilities as Gui_utilities
 import tkinter.scrolledtext as scrolledtext
 
 
-class Mainpage:
+class Mainpage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.parent = parent
 
-    def __init__(self, master):
-        self.HEIGHT = 700
-        self.WIDTH = 500
-        self.master = master
+        # From here starts the code
         self.gimbab = importer.food('Gimbab')
         self.bulgogi = importer.food('Bulgogi')
         self.kimchi = importer.food('Kimchi_Jjigae')
 
-
-
-        # A canvas for a default size while starting
-        self.canvas = tk.Canvas(self.master, height=self.HEIGHT, width=self.WIDTH)
-        self.canvas.pack()
-
         # In this frame the other objects will be added
-        self.frame = tk.Frame(self.master, bg='white')
+        self.frame = tk.Frame(self, bg='white')
         self.frame.place(relwidth=1, relheight=1)
 
-        self.image_frame = tk.Frame(self.master, bg='white')
+        self.image_frame = tk.Frame(self, bg='white')
         self.image_frame.place(relx=0.5, rely=0.2, width=400, height=450, anchor='n')
 
         # the image of the foods will be called from the get_food_image function of importer module
@@ -37,7 +32,7 @@ class Mainpage:
         #self.food_label.place(relx=0.5, rely=0.3, width=400, height=400, anchor='n')
 
         # Button to close this and go to the next page
-        self.button2 = tk.Button(self.frame, text='Show the recipe', command=self.button_clicked)
+        self.button2 = tk.Button(self.frame, text='Show the recipe', command=lambda: controller.show_frame("Secondpage"))
         # this keeps the bottom disabled at the beginninng. when user selects a food button will get enabled
         self.button2["state"] = "disabled"
         self.button2.place(relx=0.5, rely=0.9, relwidth=0.8, relheight=0.05, anchor='n')
@@ -45,7 +40,7 @@ class Mainpage:
         # Its the code for dropdown menu
         self.dropdown_selected = tk.StringVar()
         self.dropdown_selected.set("Select the desired recipe")  # Default value
-        self.food_list = (self.gimbab.foodname, 2, 3)
+        self.food_list = (self.gimbab.foodname, self.kimchi.foodname, self.bulgogi.foodname)
 
         self.list_of_food = tk.OptionMenu(self.frame, self.dropdown_selected, *self.food_list)
         # StringVar().trace() : https://kite.com/python/docs/Tkinter.StringVar.trace
@@ -64,26 +59,37 @@ class Mainpage:
         self.button2["state"] = "normal"
         val = self.dropdown_selected.get()
         print("the user chose the value {}".format(self.dropdown_selected.get()))
+
+        # Here comes the logic for the selected value in the dropdown menu
         if self.dropdown_selected.get() == self.gimbab.foodname:
             self.food_label['image'] = self.gimbab.picture
+        if self.dropdown_selected.get() == self.bulgogi.foodname:
+            self.food_label['image'] = self.bulgogi.picture
+        if self.dropdown_selected.get() == self.kimchi.foodname:
+            self.food_label['image'] = self.kimchi.picture
 
         #self.food_label['image']=self.food_image2
 
 
-class Secondpage:
+class Secondpage(tk.Frame):
 
-    def __init__(self, master, foodtype):
-        self.master = master
-        self.frame = tk.Frame(self.master)
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        #label = tk.Label(self, text="This is the start page", font=controller.title_font)
+        #label.pack(side="top", fill="x", pady=10)
+
+        self.parent = parent
+        self.frame = tk.Frame(self)
         self.frame.place(relwidth=1, relheight=1)
-        self.foodtype = foodtype
+        #self.foodtype = foodtype
 
         self.gimbab = importer.food('Gimbab')
         self.bulgogi = importer.food('Bulgogi')
         self.kimchi = importer.food('Kimchi_Jjigae')
 
 
-        print('you clicked ' + self.foodtype)
+        #print('you clicked ' + self.foodtype)
 
         # The needed stuff to make the food:
         self.needed_stuff = Gui_utilities.text_with_scrollbar(self.frame)
